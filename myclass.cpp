db090078d54050858b10493759d10636df686fb7
Adrianteri <adriateri@gmail.com>
2016-02-01 04:40:31 -0800

    new file:   myclass.cpp

---
+++ myclass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//class prototypes and function prototypes
+class Fraction
+{
+
+	int nume;
+	int denom;
+	void getData(void);
+	void showData(int n, int d);
+
+};
+
+//
